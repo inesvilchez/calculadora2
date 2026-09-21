691c114423bdfc49d557bf9e2e005b270505754f
ines <inesvilchezramirez@gmail.com>
2026-09-21 09:13:43 +0200

    Primera version: calculadora dividida en modulos y paquete

---
+++ mi_paquete/calculadora.py
@@ -0,0 +1,5 @@
+from mi_paquete import operaciones
+from mi_paquete.utils import mostrar_mensaje
+mostrar_mensaje()
+print("5 + 3 =", operaciones.sumar(5, 3))
+print("5 * 3 =", operaciones.multiplicar(5, 3))
